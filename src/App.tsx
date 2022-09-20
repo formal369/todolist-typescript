05c3764ee0bd61fbd0b67d4accbffd3008b5ceec
formal <rlaxodud9028@gmail.com>
2022-09-20 16:56:38 +0900

    showing todolist on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 새로운 Todo를 작성
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    }
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  }
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>TodoList with Typescript</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className="inputText" />
           <input type="submit" value="작성" className="submitButton" />
         </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input 
+                type="text" 
+                onChange={(e) => handleEdit(todo.id, e.target.value)} 
+                className="inputText" 
+                value={todo.inputValue}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
